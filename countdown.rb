42b4bbce486982383a4bc4478d377989625be5c5
John Brown <johnbrownsinbox@gmail.com>
2022-04-06 09:44:36 -0400

    commit tree b1b361181421a1954789b0287e31ea31fd9976f5

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
-
-def countdown
+require "pry"
+def countdown startNumber
+    while startNumber>0 do
+        puts "#{startNumber} SECOND(S)!"
+        startNumber-=1
+        
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep startNumber
+    while startNumber>0 do
+        puts "#{startNumber} SECOND(S)!"
+        startNumber-=1
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!"
+end
+
